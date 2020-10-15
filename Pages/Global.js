282d70e660d43202037bad20ace6b3939160ac57
Isfar Tousif <isfartousif2@gmail.com>
2020-10-16 00:05:15 +0600

    Day 1 End

---
+++ Pages/Global.js
@@ -0,0 +1,13 @@
+const { fstat } = require('fs')
+
+const {BrowserWindow, getCurrentWindow} = require('electron').remote
+
+var win = getCurrentWindow()
+
+// TitleBarLogic
+win.on("maximize", () => {document.getElementById("MaximizeButton").innerHTML = '<svg xmlns="http://www.w3.org/2000/svg" height="24" viewBox="0 0 24 24" width="24"><path d="M0 0h24v24H0V0z" fill="none"/><path d="M6 16h2v2c0 .55.45 1 1 1s1-.45 1-1v-3c0-.55-.45-1-1-1H6c-.55 0-1 .45-1 1s.45 1 1 1zm2-8H6c-.55 0-1 .45-1 1s.45 1 1 1h3c.55 0 1-.45 1-1V6c0-.55-.45-1-1-1s-1 .45-1 1v2zm7 11c.55 0 1-.45 1-1v-2h2c.55 0 1-.45 1-1s-.45-1-1-1h-3c-.55 0-1 .45-1 1v3c0 .55.45 1 1 1zm1-11V6c0-.55-.45-1-1-1s-1 .45-1 1v3c0 .55.45 1 1 1h3c.55 0 1-.45 1-1s-.45-1-1-1h-2z"/></svg>'})
+win.on("unmaximize", () => {document.getElementById("MaximizeButton").innerHTML = '<svg xmlns="http://www.w3.org/2000/svg" height="24" viewBox="0 0 24 24" width="24"><path d="M0 0h24v24H0V0z" fill="none"/><path d="M6 14c-.55 0-1 .45-1 1v3c0 .55.45 1 1 1h3c.55 0 1-.45 1-1s-.45-1-1-1H7v-2c0-.55-.45-1-1-1zm0-4c.55 0 1-.45 1-1V7h2c.55 0 1-.45 1-1s-.45-1-1-1H6c-.55 0-1 .45-1 1v3c0 .55.45 1 1 1zm11 7h-2c-.55 0-1 .45-1 1s.45 1 1 1h3c.55 0 1-.45 1-1v-3c0-.55-.45-1-1-1s-1 .45-1 1v2zM14 6c0 .55.45 1 1 1h2v2c0 .55.45 1 1 1s1-.45 1-1V6c0-.55-.45-1-1-1h-3c-.55 0-1 .45-1 1z"/></svg>'})
+function closeWindow(){win.close()}
+function maximizeWindow(){win.isMaximized() ? win.unmaximize() : win.maximize()}
+function minimizeWindow(){win.minimize()}
+
---
+++ main.js
@@ -0,0 +1,24 @@
+const {app, BrowserWindow, Menu} = require("electron")
+
+// Main Window
+function createMainWin(){
+    win = new BrowserWindow({
+        "width": 800,
+        "height": 500,
+        // "transparent": true,
+        "frame": false,
+        "webPreferences": {
+            "nodeIntegration": true,
+            "enableRemoteModule": true
+        }
+    })
+
+    win.loadFile("Pages/Global.html")
+    win.webContents.openDevTools()
+}
+
+// Start App
+app.on("ready", createMainWin)
+
+// Stop App (Not necessary)
+app.on("window-all-closed", () => {app.quit()}) //On Close Tasks
